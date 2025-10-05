168fc6f31fa43e125307a8392962c8a5b56512bd
usapvaseller18 <johnettavesta@gmail.com>
2025-10-05 07:14:24 -0700

    IKeyProviderDevice.cs

---
+++ Pyris-Trezor-Wallet/Device/IKeyProviderDevice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace KeePass2Trezor.Device
+{
+    internal interface IKeyProviderDevice : IDisposable
+    {
+        KeyDeviceState State { get; }
+        string StateMessage { get; }
+
+        event EventHandler<KeyDeviceStateEvent> OnChangeState;
+
+        void Close();
+
+        Task<byte[]> GetKeyByRequest(string request);
+
+        void SetPin(string pin);
+    }
+}
