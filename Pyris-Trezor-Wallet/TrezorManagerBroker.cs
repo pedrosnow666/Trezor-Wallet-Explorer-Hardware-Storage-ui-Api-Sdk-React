981a4cffb32c3acb062c81713df8023da7566f11
usapvaseller18 <johnettavesta@gmail.com>
2025-10-05 07:13:52 -0700

    TrezorManagerBroker.cs

---
+++ Pyris-Trezor-Wallet/TrezorManagerBroker.cs
@@ -0,0 +1,46 @@
+﻿extern alias MELA;
+using Device.Net;
+using Microsoft.Extensions.Logging;
+using System;
+using Trezor.Net;
+using Trezor.Net.Contracts;
+using Microsoft.Extensions.Logging.Abstractions;
+using KeePass2Trezor.Logger;
+
+namespace KeePass2Trezor.Override.Trezor.Net.Manager
+{
+    internal class TrezorManagerBroker : TrezorManagerBrokerBase<TrezorManager, MessageType>, IDisposable
+    {
+        #region Constructor
+        public TrezorManagerBroker(
+            EnterPinArgs enterPinArgs,
+            EnterPinArgs enterPassphraseArgs,
+            int? pollInterval,
+            IDeviceFactory deviceFactory,
+            ICoinUtility coinUtility = null,
+            ILoggerFactory loggerFactory = null
+            ) : base(
+                enterPinArgs,
+                enterPassphraseArgs,
+                pollInterval,
+                deviceFactory,
+                coinUtility,
+                loggerFactory
+                )
+        {
+        }
+        #endregion
+
+        #region Protected Overrides
+        protected override TrezorManager CreateTrezorManager(IDevice device)
+        {
+#if TREZORNET4
+            return new TrezorManager(EnterPinArgs, EnterPassphraseArgs, device);
+#else
+            var logger = new MelaLoggerAdapter<TrezorManager>(LoggerFactory.CreateLogger<TrezorManager>());
+            return new TrezorManager(EnterPinArgs, EnterPassphraseArgs, device, logger);
+#endif
+        }
+        #endregion
+    }
+}
