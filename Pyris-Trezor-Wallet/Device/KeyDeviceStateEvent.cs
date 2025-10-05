7d72e9ec7b4c7b32b54c8fb0da8c2785c7a53b38
usapvaseller18 <johnettavesta@gmail.com>
2025-10-05 07:14:27 -0700

    KeyDeviceStateEvent.cs

---
+++ Pyris-Trezor-Wallet/Device/KeyDeviceStateEvent.cs
@@ -0,0 +1,14 @@
+﻿namespace KeePass2Trezor.Device
+{
+    internal class KeyDeviceStateEvent
+    {
+        public KeyDeviceStateEvent(KeyDeviceState state, string message = null)
+        {
+            State = state;
+            Message = message;
+        }
+        public KeyDeviceState State { get; private set; }
+
+        public string Message { get; private set; }
+    }
+}
