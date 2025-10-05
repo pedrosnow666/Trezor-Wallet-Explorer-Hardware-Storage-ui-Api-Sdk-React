d2768a001b271daf6c7c03500382c0c2e0464cbb
usapvaseller18 <johnettavesta@gmail.com>
2025-10-05 07:13:42 -0700

    LedgerDotNetApiTests.cs

---
+++ Pyris-Trezor-Wallet/LedgerDotNetApiTests.cs
@@ -0,0 +1,15 @@
+﻿using LedgerWallet;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hardwarewallets.Net.UnitTests
+{
+    public class LedgerDotNetApiTests : UnitTestBase
+    {
+        public override async Task Initialize()
+        {
+            var ledgerClient = (await LedgerClient.GetHIDLedgersAsync()).First();
+            AddressDeriver = new LedgerDotNetApiWrapper(ledgerClient);
+        }
+    }
+}
